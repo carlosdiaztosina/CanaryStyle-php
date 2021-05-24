59e752ec2406f0f9030738d01390f2b2ecf0b9cb
daniiherreroo <danielherrero1396@gmail.com>
2021-05-24 10:02:20 +0100

    ruso

--- camisetasHombre.php
+++ camisetasHombre.php
@@ -5,6 +5,7 @@ echo '
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
@@ -19,12 +20,12 @@ View::navigation();
 $datos = DB::execute_sql('SELECT * FROM articulos WHERE tipo=1');
     echo '<h1 class="camisetastitulo">Camisetas</h1>
         <div class="center-camisetas">
-            <div class="row">';
+            <div class="camisetas">';
 
         foreach($datos as $camisetashombre){
             $id = $camisetashombre['id'];
-            echo '<div class="camisetas">';
-            echo '<div class="col">';
+            echo '<div class="">';
+            echo '<div class="">';  // Esto ya lo fixeas si ta VIVA RUSIA ?DONDE TIENES EL CSS?
                     echo "<div class='main-camiseta' onclick=\"window.location='camisetaHombre.php?id=$id'\">";
                         $img=View::imgtobase64($camisetashombre['imagen']);
                         echo "<img  src='$img' alt='camisetas-img1' /> ";                          
